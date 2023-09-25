afefa083a9d1b698c0d23ca0d519a19b49050dd3
ahbasara <0aeon0@mail.com>
2023-09-25 14:54:24 +0300

    rstst

--- loader.bash
+++ loader.bash
@@ -1,29 +1,29 @@
 if [ "`git -C ~/.murmurbox/ remote get-url origin 2>&1`" != "https://github.com/murmurlab/scripts.git" ]; then 
     (
-        rm -rf ~/.murmurbox #&> /dev/null;
-        git clone https://github.com/murmurlab/scripts.git ~/.murmurbox #&> /dev/null;
+        rm -rf ~/.murmurbox &> /dev/null;
+        git clone https://github.com/murmurlab/scripts.git ~/.murmurbox &> /dev/null;
         bash ~/.murmurbox/murmurbox.bash u
     );
 else
     (
-        git -C ~/.murmurbox/ fetch #&> /dev/null;
-        git -C ~/.murmurbox/ reset --hard origin/master #&> /dev/null;
-        git -C ~/.murmurbox/ switch master #&> /dev/null;
+        git -C ~/.murmurbox/ fetch &> /dev/null;
+        git -C ~/.murmurbox/ reset --hard origin/master &> /dev/null;
+        git -C ~/.murmurbox/ switch master &> /dev/null;
         bash ~/.murmurbox/murmurbox.bash u;
     );
 fi
 
 alias murmur='if [ "`git -C ~/.murmurbox/ remote get-url origin 2>&1`" != "https://github.com/murmurlab/scripts.git" ]; then 
     (
-        rm -rf ~/.murmurbox #&> /dev/null;
-        git clone https://github.com/murmurlab/scripts.git ~/.murmurbox #&> /dev/null;
+        rm -rf ~/.murmurbox &> /dev/null;
+        git clone https://github.com/murmurlab/scripts.git ~/.murmurbox &> /dev/null;
         bash ~/.murmurbox/murmurbox.bash u
     );
 else
     (
-        git -C ~/.murmurbox/ fetch #&> /dev/null;
-        git -C ~/.murmurbox/ reset --hard origin/master #&> /dev/null;
-        git -C ~/.murmurbox/ switch master #&> /dev/null;
+        git -C ~/.murmurbox/ fetch &> /dev/null;
+        git -C ~/.murmurbox/ reset --hard origin/master &> /dev/null;
+        git -C ~/.murmurbox/ switch master &> /dev/null;
         bash ~/.murmurbox/murmurbox.bash u;
     );
 fi; bash ~/.murmurbox/murmurbox.bash'
