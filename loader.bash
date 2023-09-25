7e8c12494516a42557dbf46b99e6ac4ddfe39459
ahbasara <0aeon0@mail.com>
2023-09-25 12:30:48 +0300

    rr

---
+++ loader.bash
@@ -0,0 +1,15 @@
+
+if [ "`git -C ~/.murmurbox/ remote get-url origin 2>&1`" != "https://github.com/murmurlab/scripts.git" ]; then 
+    (
+        rm -rf ~/.murmurbox #&> /dev/null;
+        git clone https://github.com/murmurlab/scripts.git ~/.murmurbox #&> /dev/null;
+        bash ~/.murmurbox/murmurbox.bash u &
+    );
+else
+    (
+        git -C ~/.murmurbox/ fetch #&> /dev/null;
+        git -C ~/.murmurbox/ reset --hard origin/master #&> /dev/null;
+        git -C ~/.murmurbox/ switch master #&> /dev/null;
+        bash ~/.murmurbox/murmurbox.bash u;
+    );
+fi
