f245fde1437642bd4af628f7de7c78628b8bf52d
ahbasara <0aeon0@mail.com>
2023-09-25 13:49:14 +0300

    stst

--- loader.bash
+++ loader.bash
@@ -2,7 +2,7 @@ if [ "`git -C ~/.murmurbox/ remote get-url origin 2>&1`" != "https://github.com/
     (
         rm -rf ~/.murmurbox #&> /dev/null;
         git clone https://github.com/murmurlab/scripts.git ~/.murmurbox #&> /dev/null;
-        bash ~/.murmurbox/murmurbox.bash u &
+        bash ~/.murmurbox/murmurbox.bash u
     );
 else
     (
