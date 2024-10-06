8a9994fcaef0ad6f9ddb19ed8d3470c9aab5f377
ahbasara <ahbasara@student.42istanbul.com.tr>
2024-10-06 18:48:13 +0300

    Fix DYLD_LIBRARY_PATH in apple_clang12_gnulibstdcxx_abi_installer.bash

--- modules/apple_clang12_gnulibstdcxx_abi_installer.bash
+++ modules/apple_clang12_gnulibstdcxx_abi_installer.bash
@@ -18,7 +18,8 @@ platform_inc_dir="$inc_dir/x86_64-apple-darwin19/"
 lib_dir="$libstdcxx_dir/lib/gcc/current"
 
 
-lines="stdcxx_cxxflags='-D_GLIBCXX_USE_CXX11_ABI=0 -stdlib++-isystem $inc_dir'
+lines="DYLD_LIBRARY_PATH=$lib_dir
+stdcxx_cxxflags='-D_GLIBCXX_USE_CXX11_ABI=0 -stdlib++-isystem $inc_dir'
 stdcxx_cxxflags+=' -stdlib++-isystem $platform_inc_dir'
 stdcxx_lxxflags='-Wno-deprecated -stdlib=libstdc++ -L $lib_dir'
 export stdcxx_cxxflags stdcxx_lxxflags"
