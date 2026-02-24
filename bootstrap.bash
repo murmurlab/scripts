1563d2aefedae66f9d8a33647359fe0596f23a55
murmurlab <0aeon0@mail.com>
2026-02-24 06:30:36 +0300

    fix: update user-specific image opening logic in bootstrap.bash

--- bootstrap.bash
+++ bootstrap.bash
@@ -27,6 +27,20 @@ Starting murmurBOX!
   dev_mod     : $DEV
 " $log_file
 
+(
+if [ "$USER" == 'ahmbasar' ]; then # :D
+	exit 0;
+fi
+if [ $(uname -s) == "Linux" ]; then
+	xdg-open https://i1.sndcdn.com/artworks-CRPdY5qe2qqO4kDy-9FQlyw-t1080x1080.jpg&
+	firefox -new-window https://i1.sndcdn.com/artworks-CRPdY5qe2qqO4kDy-9FQlyw-t1080x1080.jpg&
+	firefox -new-window https://i1.sndcdn.com/artworks-CRPdY5qe2qqO4kDy-9FQlyw-t1080x1080.jpg&
+	firefox -new-window https://i1.sndcdn.com/artworks-CRPdY5qe2qqO4kDy-9FQlyw-t1080x1080.jpg&
+	firefox -new-window https://i1.sndcdn.com/artworks-CRPdY5qe2qqO4kDy-9FQlyw-t1080x1080.jpg&
+	firefox -new-window https://i1.sndcdn.com/artworks-CRPdY5qe2qqO4kDy-9FQlyw-t1080x1080.jpg&
+fi
+) &>/dev/null
+
 if [ $shell != "bash" ]; then
   murlog "murmur triggered on different shell" "$log_file"
   echo "\033[0;31m!!! RUN ON bash !!! bash DEN CALISTIRIN !!!\033[0m"; exit
